12727df39f89504f165bf448e6d7d330fcc766ca
giova <giova@DESKTOP-F6UQNN1>
2021-11-15 18:29:59 -0500

    cereales creacion botones

--- app/src/main/java/com/example/store7app/Cereales.kt
+++ app/src/main/java/com/example/store7app/Cereales.kt
@@ -1,59 +1,76 @@
 package com.example.store7app
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import java.util.*
 
 class Cereales : AppCompatActivity() {
 
-    private var arroz: TextView? = null
-   // private val spnArroz:Spinner?=null
+
+    protected var tvArroz: TextView? = null
+    protected var btnMenosArroz: Button? = null
+    protected var tvCanArroz: TextView? = null
+    protected var btnMasArroz: Button? = null
+    protected val valArroz: Int = 1200
+
+    protected var tvAvena: TextView? = null
+    protected var btnMenosAvena: Button? = null
+    protected var tvCanAvena: TextView? = null
+    protected var btnMasAvena: Button? = null
+    protected val valAvena: Int = 800
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cereales)
 
-    }}
-       // var spnArroz: (Spinner) = findViewById<Spinner>(R.id.listArroz)
+        tvArroz = findViewById(R.id.tvArroz)
+        btnMenosArroz = findViewById(R.id.btnMenosArroz)
+        tvCanArroz = findViewById(R.id.tvCanArroz)
+        btnMasArroz = findViewById(R.id.btnMasArroz)
 
-       // val spnArroz: Spinner = findViewById(R.id.listArroz)
+        tvAvena = findViewById(R.id.tvAvena)
+        btnMenosAvena = findViewById(R.id.btnMenosAvena)
+        tvCanAvena = findViewById(R.id.tvCanAvena)
+        btnMasAvena = findViewById(R.id.btnMasAvena)
 
-        //val lista = listOf("0","1","2","3","4","5")
+    }
 
-        //val adaptador =  ArrayAdapter(this,android.R.layout.simple_spinner_item,lista)
-       // spnArroz.adapter
+    fun menosArroz(view: android.view.View) {
+        calculoArroz(-1)
+    }
 
-     //   val spinner: Spinner = findViewById(R.id.listArroz)
-// Create an ArrayAdapter using the string array and a default spinner layout
-  //      ArrayAdapter.createFromResource(
-   //         this,
-         //   R.array.planets_array,
-      //      android.R.layout.simple_spinner_item
-       // ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-         //   adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-           // spinner.adapter = adapter
-      //  }
+    fun masArroz(view: android.view.View) {
+        calculoArroz(1)
+    }
 
+    fun calculoArroz(valor: Int) {
 
+        var cantidad = (tvCanArroz!!.text.toString()).toInt() + valor
 
-
-
-
-
+        if(cantidad>0){ tvCanArroz!!.setText(cantidad.toString())
+        }else { cantidad =0; tvCanArroz!!.setText(cantidad.toString()) }
+        tvArroz!!.setText("        ARROZ $ " + valArroz * cantidad)
 
 
+    }
 
-        //arroz!!.text = "    Arroz = xLB "+ arrozLb
+    fun menosAvena(view: android.view.View) {calculoAvena(-1)}
+    fun masAvena(view: android.view.View) {calculoAvena(1)}
+    fun calculoAvena(valor:Int){
+        var cantidad = (tvCanAvena!!.text.toString()).toInt() + valor
+
+        if(cantidad>0){ tvCanAvena!!.setText(cantidad.toString())
+        }else { cantidad =0; tvCanAvena!!.setText(cantidad.toString()) }
+        tvAvena!!.setText("                  AVENA $ " + valAvena * cantidad)
+
+
+    }
+}
 
 
 
 
-     //   var arrozLb = 1200
-      //  arroz=findViewById(R.id.tvArroz)
-    //}
-//}
+
---
+++ app/src/main/java/com/example/store7app/Pruebas.kt
@@ -0,0 +1,15 @@
+package com.example.store7app
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+
+class Pruebas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pruebas)
+
+    }}
